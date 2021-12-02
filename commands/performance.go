b731d3e64c5da997b757eb51f196b051a904d85a
VigneshC-Jfrog <vigneshc@jfrog.com>
2021-12-02 23:16:37 +0530

    chart logic

--- commands/performance.go
+++ commands/performance.go
@@ -7,8 +7,12 @@ import (
 	"strconv"
 	"strings"
 
+	"os"
+
 	"github.com/jfrog/jfrog-cli-core/v2/common/commands"
 	"github.com/jfrog/jfrog-cli-platform-advisor/common"
+
+	"github.com/wcharczuk/go-chart/v2"
 )
 
 type Repo struct {
@@ -35,6 +39,8 @@ func getStorageSummaryAdvise() error {
 		return err
 	} else {
 		var storageSummary StorageSummary
+		var My_map = make(map[string]float64)
+		var values []chart.Value
 		var url = serverDetails.GetArtifactoryUrl() + "api/storageinfo"
 		httpRequest := &common.HttpRequest{ReqUrl: url, ReqType: "GET", AuthUser: serverDetails.GetUser(), AuthPass: serverDetails.GetPassword()}
 		var response = common.MakeHTTPCall(*httpRequest)
@@ -48,10 +54,31 @@ func getStorageSummaryAdvise() error {
 		for i := 0; i < len(storageSummary.Repo); i++ {
 			var size_type = strings.Split(storageSummary.Repo[i].UsedSpace, " ")[1]
 			var used_size, _ = strconv.ParseFloat(strings.Split(storageSummary.Repo[i].UsedSpace, " ")[0], 64)
-			if (size_type == "GB" && used_size > 5) || size_type == "TB" {
+			if (size_type == "GB" && used_size > 0.9) || size_type == "TB" {
 				fmt.Println("Reposiroty " + storageSummary.Repo[i].RepoKey + " has " + storageSummary.Repo[i].UsedSpace + ". Please split the content to different repositories.")
+				My_map[storageSummary.Repo[i].RepoKey+"("+size_type+")"] = used_size
+
+				fmt.Println("My_map: ", My_map)
 			}
 		}
+		for l, v := range My_map {
+			values = append(values, chart.Value{Label: l, Value: float64(v)})
+		}
+		graph := chart.BarChart{
+			Title: "Test Bar Chart",
+			Background: chart.Style{
+				Padding: chart.Box{
+					Top: 40,
+				},
+			},
+			Height:   512,
+			BarWidth: 60,
+			Bars:     values,
+		}
+
+		f, _ := os.Create("output.png")
+		defer f.Close()
+		graph.Render(chart.PNG, f)
 	}
 	return nil
 }
--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/jfrog/jfrog-cli-core/v2 v2.3.0
 	github.com/jfrog/jfrog-client-go v1.4.0
 	github.com/stretchr/testify v1.7.0
+	github.com/wcharczuk/go-chart/v2 v2.1.0
 )
